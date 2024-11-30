0367460254246da5873be60a45ee7a87ca7fea46
KawalaE <edyta.wer.kawala@gmail.com>
2024-11-30 14:11:06 +0100

    Display mocked data in a table for now

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../model/product.type';
 
 @Component({
   selector: 'app-product',
   imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
 })
 export class ProductComponent {
-
+  product = input.required<Product>();
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { ProductComponent } from '../components/product/product.component';
+import { Product } from '../model/product.type';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ProductComponent, NgFor],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  productsService = inject(ProductsService);
+  productItems = signal<Array<Product>>([]);
 
+  ngOnInit(): void {
+    this.productItems.set(this.productsService.mockProducts);
+  }
 }
