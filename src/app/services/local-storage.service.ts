eb65b9aa4db3e064bc8ca8c5e7c59fd66b11d569
KawalaE <edyta.wer.kawala@gmail.com>
2024-12-01 18:03:54 +0100

    Intialize mock data into a local storage

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Campaign } from '../model/campaign.type';
+import { CampaignsService } from './campaigns.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService {
   private storageKey = 'campaigns';
-  constructor() {}
 
+  constructor(private campaignsService: CampaignsService) {
+    this.initializeLocalStorage();
+  }
+  private initializeLocalStorage(): void {
+    if (!localStorage.getItem(this.storageKey)) {
+      const mockCampaigns = this.campaignsService.mockCampaigns;
+      this.saveCampaigns(mockCampaigns);
+    }
+  }
   getCampaigns(): Campaign[] {
     const storedData = localStorage.getItem(this.storageKey);
     return storedData ? JSON.parse(storedData) : [];
