2bb52d47f9712612e4d4a2d620eceef11c7a93a1
KawalaE <edyta.wer.kawala@gmail.com>
2024-12-02 12:19:58 +0100

    Modify and style header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  navigateToHome(): void {
+    this.router.navigate(['/']);
+  }
+}
